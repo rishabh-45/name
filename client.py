9cc7923b884ebdb4a5c5e17708c65ac3a1ec70cf
TechyNewbie <hawkeyesmohak@gmail.com>
2020-09-18 20:48:18 +0530

    Allow deploying other branches

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
 import subprocess
 import os
-GITHUB_REPO_LINK = os.environ.get("GITHUB_REPO_LINK", "https://github.com/justaprudev/The-TG-Bot")
+GITHUB_REPO_LINK = os.environ.get("GITHUB_REPO_LINK", "https://github.com/justaprudev/The-TG-Bot v3")
+repo = GITHUB_REPO_LINK.split()[0]
+branch = GITHUB_REPO_LINK.split()[1]
 print("Downloading latest version of The-TG-Bot..")
-subprocess.run(f"git clone {GITHUB_REPO_LINK} git && mv git/* . && rm -rf git", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
-print("Starting process with command `python3 -m thetgbot`")
-print("State changed from starting to up")
+subprocess.run(f"git clone -b {branch} {repo} git && mv git/* . && rm -rf git", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
+print("Starting The-TG-Bot")
 subprocess.run("python3 -m thetgbot", shell=True)
